99ab2935e12f35c1749215dedbd964e9883f182f
Jacky So <jacky.so@24x7classroom.com>
2022-08-09 17:31:16 +1000

    first commit :tada:

---
+++ README.md
@@ -0,0 +1,1 @@
+# Jacky So's blog
---
+++ config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  // the name of your website
+  title: 'Jacky So',
+  // the description of the website (eg. what shows on Google)
+  description: "Jacky So's blog",
+  // a short bio shown at the bottom of your blog posts
+  // It should complete the sentence: Written by Jacky So ...
+  shortBio: '',
+  // a longer bio showing on the landing page of the blog
+  bio: ``,
+  author: 'Jacky So',
+  githubUrl: 'https://github.com/JackySo-24x7classroom/public-resources',
+  // replace this by the url where your website will be published
+  siteUrl: 'http://localhost:8000',
+  social: {
+    // leave the social media you do not want to appear as empty strings
+    twitter: '',
+    medium: '@24x7classroom',
+    facebook: '',
+    github: '',
+    linkedin: '',
+    instagram: '',
+  },
+  // GoatCounter code to enable analytics. See https://github.com/mathieudutour/medium-to-own-blog/tree/master/docs#analytics-tracking-page-views-etc
+  goatCounterCode: null,
+}
